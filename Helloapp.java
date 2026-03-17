b41b02b002a7e9600cf865e52da1db7507f1d988
Szyrex <sachinarunachalam54@gmail.com>
2026-03-17 14:15:06 +0530

    Coded UC3 to print Hello world

--- Helloapp.java
+++ Helloapp.java
@@ -1,14 +1,24 @@
 public class Helloapp {
-    //uc2
+    //uc3
+
+
 
     public static void main(String[] args) {
 
-        String name = args[0];
-        System.out.println("Hello " + name + "!");
+        // Default name
+        String name = "World";
 
+        // Check if a name is provided as a command-line argument
+        if (args.length > 0) {
+            name = args[0]; // Use the provided name
+        }
+
+        System.out.println("Hello, " + name + "!");
     }
-
-
-
 }
+
+
+
+
+
 
