dfde6f712e8fb7b542bc75deaf18bcf7b790ecff
Szyrex <sachinarunachalam54@gmail.com>
2026-03-18 15:00:39 +0530

    Add files via upload

--- level1.java
+++ level1.java
@@ -1,39 +1,29 @@
 import java.util.Scanner;
+//q3
 public class level1 {
-    \\q1
-    
-
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        int[] ages = new int[10];
+        
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
 
-        // Taking input
-        System.out.println("Enter ages of 10 students:");
-        for (int i = 0; i < ages.length; i++) {
-            System.out.print("Enter age of student " + (i + 1) + ": ");
-            ages[i] = sc.nextInt();
+        
+        int[] table = new int[10];
+
+        
+        for (int i = 0; i < 10; i++) {
+            table[i] = number * (i + 1);
         }
 
-        System.out.println("\nResult:");
-
-        // Checking eligibility
-        for (int i = 0; i < ages.length; i++) {
-
-            if (ages[i] < 0) {
-                System.out.println("Invalid age");
-            }
-            else if (ages[i] >= 18) {
-                System.out.println("Student with age " + ages[i] + " can vote.");
-            }
-            else {
-                System.out.println("Student with age " + ages[i] + " cannot vote.");
-            }
+        
+        System.out.println("\nMultiplication Table:");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(number + " * " + (i + 1) + " = " + table[i]);
         }
 
         sc.close();
     }
-
 }
